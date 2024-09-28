f77a1d68ef84f13046954f91e76fab7c65134205
Anderson De Souza <65574525+souzaseven@users.noreply.github.com>
2024-09-27 23:00:32 -0400

    tabela ascii

--- Projetos/Pessoal/Texto para Ascii/script.js
+++ Projetos/Pessoal/Texto para Ascii/script.js
@@ -1,13 +1,24 @@
-document.getElementById('textInput').addEventListener('input', () => {
-    const textInput = document.getElementById('textInput').value;
-    const output = document.getElementById('output');
+  const textInput = document.getElementById('textInput');
+        const output = document.getElementById('output');
+        const clearButton = document.getElementById('clearButton');
 
-    if (textInput) {
-        const asciiCodes = Array.from(textInput)
-            .map(char => `${char}: ${char.charCodeAt(0)}`)
-            .join('\n');
-        output.textContent = asciiCodes;
-    } else {
-        output.textContent = ''; // Limpa a área de saída quando o campo está vazio
-    }
-});
+        textInput.addEventListener('input', () => {
+            const inputValue = textInput.value;
+
+            if (inputValue) {
+                const asciiCodes = Array.from(inputValue)
+                    .map(char => `${char}: ${char.charCodeAt(0)}`)
+                    .join('\n');
+                output.textContent = asciiCodes;
+                clearButton.style.display = 'inline-block'; // Mostra o botão de limpar quando há conteúdo
+            } else {
+                output.textContent = ''; // Limpa a área de saída quando o campo está vazio
+                clearButton.style.display = 'none'; // Esconde o botão de limpar se não houver conteúdo
+            }
+        });
+
+        clearButton.addEventListener('click', () => {
+            textInput.value = '';
+            output.textContent = '';
+            clearButton.style.display = 'none'; // Esconde o botão de limpar
+        });
